6b31931b7dce7c44fa7a2f5100258deda16beccc
firdawse <85738021+firdawse@users.noreply.github.com>
2022-12-18 13:36:51 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, flash
+import model as m
+
+app = Flask(__name__)
+
+app.secret_key = "manbearpig_MUDMAN888"
+
+@app.route("/hello")
+def index():
+	return render_template("welcome.html")
+
+@app.route("/result1", methods=['POST', 'GET'])
+def result1():
+	flash(m.movie(request.form['name_input']))
+	return render_template("index.html")
+
+@app.route('/UserBased')
+def UserBased():
+	return render_template('index.html')
+
+@app.route('/kmeans')
+def kmeans():
+	return render_template('kmeans.html')
+
+@app.route("/result2", methods=['POST', 'GET'])
+def result2():
+	flash(m.movie(request.form['name_input']))
+	return render_template("kmeans.html")
+
+@app.route('/knn')
+def knn():
+	return render_template('knn.html')
+
+@app.route("/result3", methods=['POST', 'GET'])
+def result3():
+	flash(m.movie(request.form['name_input']))
+	return render_template("knn.html")
+
+@app.route('/mlp')
+def mlp():
+	return render_template('mlp.html')
+
+@app.route("/result4", methods=['POST', 'GET'])
+def result4():
+	flash(m.movie(request.form['name_input']))
+	return render_template("mlp.html")
+
+
+if __name__ == '__main__':
+    app.run(port=4000)
+
---
+++ model.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+
+movie = pd.read_csv("movie.csv")
+movie = movie.loc[:,["movieId","title"]]
+rating = pd.read_csv("rating.csv")
+rating = rating.loc[:,["userId","movieId","rating"]]
+# then merge movie and rating data
+data = pd.merge(movie,rating)
+data = data.iloc[:1000000,:]
+# lets make a pivot table in order to make rows are users and columns are movies. And values are rating
+pivot_table = data.pivot_table(index = ["userId"],columns = ["title"],values = "rating")
+
+def movie(input):
+    movie_watched = pivot_table[input]
+    similarity_with_other_movies = pivot_table.corrwith(movie_watched)# find correlation between "Bad Boys (1995)" and other movies
+    # find correlation between "Bad Boys (1995)" and other movies
+    return similarity_with_other_movies.head().index.tolist()[0]+ '\n'+ similarity_with_other_movies.head().index.tolist()[1] +'\n'+ similarity_with_other_movies.head().index.tolist()[2]+'\n'+ similarity_with_other_movies.head().index.tolist()[3]+'\n'+ similarity_with_other_movies.head().index.tolist()[4]
